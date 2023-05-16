c8c0602f5218f55e3d2c18627746ae8e3e043b67
RamaReksotinoyo <ramareksotinoyo@gmail.com>
2023-05-17 00:09:37 +0700

    function calculate probability

--- prob.go
+++ prob.go
@@ -4,6 +4,19 @@ func Prob() string {
 	return "This is probability module."
 }
 
+type Probability struct {
+	Value float64
+}
+
+func CalculateProbability(event *float64, sampleSpace *float64) *Probability {
+	if *sampleSpace == 0 {
+		panic("Sample space cannot be zero.")
+	}
+
+	value := *event / *sampleSpace
+	return &Probability{Value: value}
+}
+
 
 
 
