ba1f5394277c13080f4f4f94dba1596e8137205d
RamaReksotinoyo <ramareksotinoyo@gmail.com>
2023-05-17 00:29:22 +0700

    instance object prob

--- prob.go
+++ prob.go
@@ -8,6 +8,14 @@ type Probability struct {
 	Value float64
 }
 
+// instance dari object probability
+func InstProbability(value float64) *Probability {
+	if value < 0 || value > 1 {
+		panic("Probability value must be between 0 and 1.")
+	}
+	return &Probability{Value: value}
+}
+
 func CalculateProbability(event *float64, sampleSpace *float64) *Probability {
 	if *sampleSpace == 0 {
 		panic("Sample space cannot be zero.")
@@ -16,7 +24,3 @@ func CalculateProbability(event *float64, sampleSpace *float64) *Probability {
 	value := *event / *sampleSpace
 	return &Probability{Value: value}
 }
-
-
-
-
